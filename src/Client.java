23ca44fc625409013cf97f8f5417de86b568920b
Azargul Nazari <nazariazargul@gmail.com>
2018-04-15 09:44:45 +0300

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -1,6 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication2;
+
 
 import java.io.IOException;
-import java.math.BigInteger;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
